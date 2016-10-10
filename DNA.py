420b061ef0bd448416aec65fe0a51e10215e8848
kuljat991 <kuljat991@gmail.com>
2016-10-10 17:14:39 +0200

    Genetic Algorithm. Shakespear experiment with monkey. Not finish.

---
+++ DNA.py
@@ -0,0 +1,61 @@
+from random import randint
+
+# A class to describe a pseudo-DNA, i.e. genotype
+#   Here, a virtual organism's DNA is an array of character.
+#   Functionality:
+#      -- convert DNA into a string
+#      -- calculate DNA's "fitness"
+#      -- mate DNA with another set of DNA
+#      -- mutate DNA
+
+def newCharacter ():
+    c = randint(63,122)
+    if c == 63:
+        c = 32;
+    if c ==64: 
+        c = 46;
+    
+    return chr(c)
+
+class DNA:
+    def __init__(self,num):
+#   The genetic sequence
+        self.genes = []
+        self.fitness = 0.
+        for  i in range (0, num):
+            self.genes.append(newCharacter());  # Pick from range of chars
+#        print self.genes        
+        self.word = ''.join(self.genes)
+
+#   Fitness function (returns floating point % of "correct" characters)
+    
+    def calcFitness (self,target):        
+        targetList = list(target)        
+        score = 0.;
+        for i in range (len(target)):
+            if (self.genes[i] == targetList[i]):  
+                score = score + 1.;
+                
+        self.fitness = score / float(len(target))
+#        print self.fitness
+
+#    Crossover
+    def crossover (self, partner):
+        child = DNA(len(self.genes))
+        midpoint = int(randint(0, len(self.genes))) # Pick a midpoint
+    
+#    Half from one, half from the other
+        for i in range (len(self.genes)) : 
+            if (i > midpoint):
+                child.genes[i] = self.genes[i]
+            else:
+              child.genes[i] = partner.genes[i]
+        return child
+        
+
+#   Based on a mutation probability, picks a new random character
+    def mutate( self, mutationRate):
+        for i in range (len(self.genes)):
+            chanceForMutation = float(randint(0,100))/100.
+            if (chanceForMutation < mutationRate):
+                self.genes[i] = newCharacter()
---
+++ Population.py
@@ -0,0 +1,90 @@
+from DNA import DNA
+from random import randint
+from scipy.interpolate import interp1d
+
+#   A class to describe a population of virtual organisms
+#   In this case, each organism is just an instance of a DNA object
+
+class Population:
+    def __init__(self, p, m, num):
+        self.maxFitness = 0.0
+        self.population = []    #   Array to hold the current population
+        self.matingPool = []    #   ArrayList which we will use for our "mating pool"
+        self.generations = 0    #   Number of generations
+        self.finished = False   #   Are we finished evolving?
+        self.target = p         #   Target phrase
+        self.mutationRate = m   #   Mutation rate
+        self.perfectScore = 1.
+        
+        for i in range (num):
+            x = DNA(len (self.target))
+            self.population.append(x)
+            
+#        self.calcFitness()
+#        self.naturalSelection()
+#        mapFitness = interp1d ([0, self.maxFitness],[ 0, 100])
+#        for i in range (len(self.population)):
+#            fitness = mapFitness(self.population[i].fitness)
+#            n = int(fitness)
+#            for i in range (n):
+#                self.matingPool.append(self.population[i])
+#        print len(self.matingPool)
+#        self.generate()
+#        self.evaluate()
+    
+    # Fill our fitness array with a value for every member of the population
+    def calcFitness (self):
+        for i in range (len(self.population)):
+            self.population[i].calcFitness(self.target)
+#            print self.population[i].fitness
+    
+    # Generate a mating pool
+    def naturalSelection (self):
+        self.matingPool = []
+        for i in range (len(self.population)): 
+            if (self.population[i].fitness > self.maxFitness):
+                self.maxFitness = self.population[i].fitness;
+#                print self.maxFitness
+        mapFitness = interp1d ([0, self.maxFitness],[ 0, 100])
+        for i in range (len(self.population)):
+            fitness = mapFitness(self.population[i].fitness)
+            n = int(fitness)
+            for j in range (n):
+                self.matingPool.append(self.population[i])
+#        print len(self.matingPool)
+    
+    # Create a new generation
+    def generate (self):
+    #Refill the population with children from the mating pool
+        for i in range (len(self.population)):
+            a = int(randint(0, len(self.matingPool)-1))
+            b = int(randint(0, len(self.matingPool)-1))
+#            print len(self.population)
+#            print len(self.matingPool)
+            partnerA = self.matingPool[a]
+            partnerB = self.matingPool[b]
+            child = partnerA.crossover(partnerB)
+            child.mutate(self.mutationRate)
+            self.population[i] = child
+    
+        self.generations = self.generations+1;
+    
+    # Compute the current "most fit" member of the population
+    def evaluate (self):
+        worldrecord = 0.
+        index = 0
+        for i in range (len(self.population)):
+            if (self.population[i].fitness > worldrecord):
+                index = i
+                worldrecord = self.population[i].fitness
+        
+        self.best = self.population[index].word
+        print self.best
+#        print len(self.best)
+        
+        if (worldrecord == self.perfectScore):
+            self.finished = True
+        
+    def isFinished (self):
+        return self.finished
+    
